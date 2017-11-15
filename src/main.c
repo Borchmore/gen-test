52da4beb7624ac53f55d2f5ef90d610753faf62b
Borchmore <dmoney1028@yahoo.com>
2017-11-14 22:54:47 -0500

    Fourth commit: 11/14/2017. Added collision detection

--- src/main.c
+++ src/main.c
@@ -31,6 +31,7 @@ static void bgAction();
 static void enemyAction();
 //currently unused
 static void otherAction();
+static void collisionAction(u16 sp1, u16 val1, u16 sp2);
 
 //sprites structures (pointer of Sprite)
 Sprite* ship;
@@ -41,17 +42,23 @@ Sprite* other[20];
 //global variable declarations
 fix16 shipPosX;
 fix16 shipPosY;
-s16 shotPosX[24];
-s16 shotPosY[24];
+s16 shipHP; //maybe change these HP variables to be a data attribute for each sprite?
+fix16 shotPosX[24];
+fix16 shotPosY[24];
+s16 shotHP[24]; //?
 fix16 enemyPosX[35];
 fix16 enemyPosY[35];
+s16 enemyHP[35]; //?
 s16 bgaScroll;
 s16 bgbScroll;
 fix16 shipVSpeed;
 fix16 shipHSpeed;
-u8 lockout;
-//used as a test; enemies spawn once every second
-u8 test;
+u16 lockout;
+u16 test; //used as a test; enemies spawn once every second
+u16 i; //used as an incremental counter in a couple methods, might change name to "increment" when I get less lazy
+u16 sp1; //identifies the first sprite in the collisionAction function
+u16 sp2; //"    "   "   " second sprite "   "   "   "   "   "   "   "
+u16 val1; //identifies array value for sp1
 
 int main(){
 
@@ -91,6 +98,7 @@ int main(){
 
     // init ship sprite
     ship = SPR_addSprite(&ship_sprite, fix16ToInt(shipPosX), fix16ToInt(shipPosY), TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
+    shipHP = 1;
     SPR_update();
 
     // prepare palettes
@@ -137,7 +145,7 @@ static void shipAction(){
         }
     }
     else {
-        if((shipVSpeed < SHIP_ACCEL) & (shipVSpeed > (-1)*SHIP_ACCEL)){
+        if((shipVSpeed < SHIP_ACCEL) && (shipVSpeed > (-1)*SHIP_ACCEL)){
             shipVSpeed = 0;
         }
         else{
@@ -162,7 +170,7 @@ static void shipAction(){
         }
     }
     else {
-        if((shipHSpeed < SHIP_ACCEL) & (shipHSpeed > (-1)*SHIP_ACCEL)){
+        if((shipHSpeed < SHIP_ACCEL) && (shipHSpeed > (-1)*SHIP_ACCEL)){
             shipHSpeed = 0;
         }
         else{
@@ -197,8 +205,15 @@ static void shipAction(){
         shipVSpeed = 0;
     }
 
+    collisionAction(0, 0, 2);
+
+    if(shipHP == 0){
+        SPR_setVisibility(ship, HIDDEN);
+    }
+    else{
     //sets position
-    SPR_setPosition(ship, fix16ToInt(shipPosX), fix16ToInt(shipPosY));
+        SPR_setPosition(ship, fix16ToInt(shipPosX), fix16ToInt(shipPosY));
+    }
 
 }
 
@@ -216,8 +231,7 @@ static void bgAction(){
 
 static void shotAction(){
 
-    u8 i = 0;
-
+    i = 0;
     //first checks to see if the user is locked out from shot input
     if(lockout == 0){
         //checks if B button is pressed
@@ -225,13 +239,14 @@ static void shotAction(){
             //searches shots[] array from open spot
             while(i < 24){
                 if(shots[i] == NULL){
-                    shotPosX[i] = fix16ToInt(shipPosX) + 24;
-                    shotPosY[i] = fix16ToInt(shipPosY) + 4;
-                    shots[i] = SPR_addSprite(&shot_sprite, shotPosX[i], shotPosY[i], TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
+                    shotPosX[i] = shipPosX + FIX16(24);
+                    shotPosY[i] = shipPosY + FIX16(4);
+                    shotHP[i] = 1;
+                    shots[i] = SPR_addSprite(&shot_sprite, fix16ToInt(shotPosX[i]), fix16ToInt(shotPosY[i]), TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
                     lockout = 19;
                     i = 24;
                 }
-                i++;
+                ++i;
             }
         }
     }
@@ -245,13 +260,14 @@ static void shotAction(){
     //sets position for each non-null element of the shots[] array
     while(i < 24){
         if (shots[i] != NULL){
-            if(shotPosX[i] > 320){
+            collisionAction(1, i, 2);
+            if(shotPosX[i] > FIX16(320) || (shotHP[i] == 0)){
                 SPR_releaseSprite(shots[i]);
                 shots[i] = NULL;
             }
             else{
-                shotPosX[i] = shotPosX[i] + 8;
-                SPR_setPosition(shots[i], shotPosX[i], shotPosY[i]);
+                shotPosX[i] = shotPosX[i] + FIX16(8);
+                SPR_setPosition(shots[i], fix16ToInt(shotPosX[i]), fix16ToInt(shotPosY[i]));
             }
         }
         ++i;
@@ -261,13 +277,14 @@ static void shotAction(){
 
 static void enemyAction(){
 
-    u8 i = 0;
+    i = 0;
 
     if(test == 0){
         while(i < 35){
             if(enemies[i] == NULL){
                 enemyPosX[i] = FIX16(320);
                 enemyPosY[i] = FIX16(random() % 224);
+                enemyHP[i] = 1;
                 enemies[i] = SPR_addSprite(&enemy_sprite, enemyPosX[i], enemyPosY[i], TILE_ATTR(PAL3, FALSE, FALSE, FALSE));
                 i = 35;
                 test = 59;
@@ -281,7 +298,7 @@ static void enemyAction(){
 
     while(i < 35){
         if (enemies[i] != NULL){
-            if(enemyPosX[i] < FIX16(-32)){
+            if((enemyPosX[i] < FIX16(-32)) || (enemyHP[i] == 0)){
                 SPR_releaseSprite(enemies[i]);
                 enemies[i] = NULL;
             }
@@ -296,3 +313,51 @@ static void enemyAction(){
 }
 
 static void otherAction(){}
+
+static void collisionAction(u16 sp1, u16 val1, u16 sp2){
+
+    //the values in the header correspond to sprite types; 0 = ship, 1 = shot, 2 = enemy
+    //this will have to change when as the game gets more complex to accommodate non-binary HP and damage values
+
+    u8 i = 0;
+
+    if(sp1 == 0){
+        if(sp2 == 2){
+            while(i < 35){
+                if(((enemyPosX[i] >= shipPosX) && (enemyPosX[i] <= shipPosX +FIX16(32))) || ((shipPosX >= enemyPosX[i]) && (shipPosX <= enemyPosX[i] +FIX16(32)))){
+                    //Fuck me, that's a long ass if statement! Oh shit now we have to do it again!! (or make the above if statement even longer)
+                    if(((enemyPosY[i] >= shipPosY) && (enemyPosY[i] <= shipPosY +FIX16(16))) || ((shipPosY >= enemyPosY[i]) && (shipPosY <= enemyPosY[i] +FIX16(16)))){
+                        shipHP = 0;
+                        enemyHP[i] = 0;
+                        i = 35;
+                    }
+                }
+                ++i;
+            }
+        }
+    }
+
+    i = 0;
+
+    if(sp1 == 1){
+        if(sp2 == 2){
+            while(i < 35){
+                if(((enemyPosX[i] >= shotPosX[val1]) && (enemyPosX[i] <= shotPosX[val1] +FIX16(16))) || ((shotPosX[val1] >= enemyPosX[i]) && (shotPosX[val1] <= enemyPosX[i] +FIX16(32)))){
+                    //Fuck me, that's a long ass if statement! Oh shit now we have to do it again!! (or make the above if statement even longer)
+                    if(((enemyPosY[i] >= shotPosY[val1]) && (enemyPosY[i] <= shotPosY[val1] +FIX16(8))) || ((shotPosY[val1] >= enemyPosY[i]) && (shotPosY[val1] <= enemyPosY[i] +FIX16(16)))){
+                        shotHP[val1] = 0;
+                        enemyHP[i] = 0;
+                        i = 35;
+                    }
+                }
+                ++i;
+            }
+        }
+    }
+
+    /*may need to be implemented when "other" objects get implemented
+    if(sp1->status == enemies->status){
+
+    } but until then...*/
+}
+
